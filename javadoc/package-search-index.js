935bcd8c41b8c1032893d6c2ed6df7b3267243a6
JuniorJacki <JuniorJacki@users.noreply.github.com>
2026-02-03 16:58:48 +0000

    Deploying to main from @ JuniorJacki/RemoteBrick@41d4106580454a3287eebcd8e69f1d17b68c9189 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.juniorjacki.remotebrick"},{"l":"de.juniorjacki.remotebrick.devices"},{"l":"de.juniorjacki.remotebrick.types"},{"l":"de.juniorjacki.remotebrick.utils"}];updateSearchResults();
